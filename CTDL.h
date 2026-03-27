bbd792aa424b32db6568baa61aa5469ade691183
BaoDuc <baoduc050706@gmail.com>
2026-03-27 07:41:25 +0700

    fixed

--- CTDL.h
+++ CTDL.h
@@ -85,6 +85,6 @@ void ThongKeSoLuotBay(PTRCB head, DSMayBay dsMB, TK kq[]);
 void SapXepGiamDanTheoSoLuot(TK kq[], int n);
 bool TrungNgay(DateTime a, int ngay, int thang, int nam);
 bool DatVe(PTRCB head, char maCB[], int soVe, char cmnd[]);
-=======
+
 typedef nodeHK* TreeHK;
 
